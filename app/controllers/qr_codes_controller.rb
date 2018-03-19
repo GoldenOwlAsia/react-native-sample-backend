eb531d09a60471ec168d634a77d05eb7c68d150a
John Wilkinson <jcwilk@gmail.com>
2018-03-20 06:29:54 +0800

    Add some todos and the barebones of the qr model

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -1,4 +1,8 @@
 class QrCodesController < ApplicationController
   def forward
+    #TODO - QR codes will contain a URL that will lead to this action
+    # the action will parse out relevant data from the url to figure out which qr code it came from
+    # it will send tracking data (skip this for now but plan for it with a placeholder)
+    # it will then redirect to the product listing (for now let's say it's a url hardcoded into the qr code model)
   end
 end
---
+++ app/models/qr_code.rb
@@ -0,0 +1,3 @@
+class QRCode < ApplicationRecord
+  #TODO - this should get pulled up by the controller and used to determine tracking data and a redirect location
+end
