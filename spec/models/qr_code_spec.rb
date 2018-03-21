b63abdeba4bcaad82c3ebdef73aff5cf3b6be59f
Justin Nguyen <justin.ntt@gmail.com>
2018-03-21 18:54:42 +0000

    add model spec facilities

--- spec/models/qr_code_spec.rb
+++ spec/models/qr_code_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QRCode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:qr_code) { build(:qr_code) }
+
+  it { should be_valid }
 end
