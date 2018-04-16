cd9cc39c3b4ba4bb7a4cefb067d391a0782b5f85
Justin Nguyen <justin.ntt@gmail.com>
2018-04-16 17:01:45 +0700

    add graphql

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   resources :landlords
   resources :shops
   resources :products
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  post "/graphql", to: "graphql#execute"
   get 'qr_codes/forward/:code', to: 'qr_codes#forward', as: :forward
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
