8055108c633c3df2ac5cb124c712ab6f0bba9aec
Justin Nguyen <justin.ntt@gmail.com>
2018-04-16 17:52:59 +0700

    config graphql types

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   resources :shops
   resources :products
 
+  get 'qr_codes/forward/:code', to: 'qr_codes#forward', as: :forward
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
   post "/graphql", to: "graphql#execute"
-  get 'qr_codes/forward/:code', to: 'qr_codes#forward', as: :forward
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
