01ff72230400c2175af6adb04bee2a556a363da4
Justin Nguyen <justin.ntt@gmail.com>
2018-04-16 18:14:52 +0700

    enable on prod

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   get 'qr_codes/forward/:code', to: 'qr_codes#forward', as: :forward
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if Rails.env.development?
+  # if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  # end
 
   post "/graphql", to: "graphql#execute"
 end
